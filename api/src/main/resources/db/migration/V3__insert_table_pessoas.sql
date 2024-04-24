19ebbbc0fd35bc05a51eaee8b8c5881ccfb7374a
Vinicius Pretti <vinicius.pretti2@hotmail.com>
2024-04-24 10:37:52 -0300

    criado tabelas e inserts

---
+++ api/src/main/resources/db/migration/V3__insert_table_pessoas.sql
@@ -0,0 +1,5 @@
+INSERT INTO pessoas (nome_completo, data_nascimento) VALUES ('Vinicius Daniel', '1979-12-25');
+INSERT INTO pessoas (nome_completo, data_nascimento) VALUES ('Marcos Aurelio', '1995-10-08');
+INSERT INTO pessoas (nome_completo, data_nascimento) VALUES ('Ana Clara da Silva', '2000-09-01');
+INSERT INTO pessoas (nome_completo, data_nascimento) VALUES ('Juliana Aparecida', '1983-06-20');
+INSERT INTO pessoas (nome_completo, data_nascimento) VALUES ('João Paulo Cordeiro', '1953-02-12');
---
+++ api/src/main/resources/db/migration/V4__insert_table_enderecos.sql
@@ -0,0 +1,17 @@
+INSERT INTO enderecos (cep, cidade, estado, is_principal, logradouro, numero, pessoa_id)
+VALUES ('12345678', 'São Paulo', 'SP', true, 'Rua Teste', '123', 1);
+
+INSERT INTO enderecos (cep, cidade, estado, is_principal, logradouro, numero, pessoa_id)
+VALUES ('45678901', 'Rio de Janeiro', 'RJ', false, 'Avenida Teste', '456', 2);
+
+INSERT INTO enderecos (cep, cidade, estado, is_principal, logradouro, numero, pessoa_id)
+VALUES ('78901234', 'Belo Horizonte', 'MG', false, 'Praça Teste', '789', 3);
+
+INSERT INTO enderecos (cep, cidade, estado, is_principal, logradouro, numero, pessoa_id)
+VALUES ('23456789', 'Porto Alegre', 'RS', true, 'Alameda Teste', '234', 4);
+
+INSERT INTO enderecos (cep, cidade, estado, is_principal, logradouro, numero, pessoa_id)
+VALUES ('56789012', 'Salvador', 'BA', false, 'Travessa Teste', '567', 5);
+
+INSERT INTO enderecos (cep, cidade, estado, is_principal, logradouro, numero, pessoa_id)
+VALUES ('89012345', 'Curitiba', 'PR', false, 'Ponte Teste', '890', 1);
